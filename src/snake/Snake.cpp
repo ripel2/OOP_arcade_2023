5fbc467dd081a37d1340761f2f2a8799ef15db2c
Lucas Hauszler <lucas.hauszler@epitech.eu>
2023-04-03 14:10:50 +0200

    feat(snake): add score and title

--- src/snake/Snake.cpp
+++ src/snake/Snake.cpp
@@ -8,7 +8,7 @@
 #include "Snake.hpp"
 
 acd::Snake::Snake()
-    : AGameModule(), _map(31, 31), _isPaused(false), _direction(0), _isGameOver(false),
+    : AGameModule(), _map(31, 32), _isPaused(false), _direction(0), _isGameOver(false),
     _movedOnce(false), _food(0, 0), _lastUpdate(std::chrono::system_clock::now())
 {
     _isPaused = false;
@@ -26,14 +26,28 @@ acd::Snake::Snake()
     char foodchars[2] = {'O', 'O'};
     foodBlock.get()->loadTexturesNcurses(Color::WHITE, Color::GREEN, foodchars);
     _blocks.push_back(std::move(foodBlock));
-    for (std::size_t y = 0; y < 31; y++) {
+    for (std::size_t x = 0; x < 31; x++) {
+        _map.setBlock(x, 1, *_blocks[1].get());
+        _map.setBlock(x, 31, *_blocks[1].get());
+    }
+    for (std::size_t y = 1; y < 32; y++) {
         _map.setBlock(0, y, *_blocks[1].get());
         _map.setBlock(30, y, *_blocks[1].get());
     }
-    for (std::size_t x = 0; x < 31; x++) {
-        _map.setBlock(x, 0, *_blocks[1].get());
-        _map.setBlock(x, 30, *_blocks[1].get());
-    }
+    std::unique_ptr<ATextBlock> score = std::make_unique<ATextBlock>();
+    score.get()->setText("Score: 0");
+    score.get()->setColor(Color::BLACK);
+    score.get()->setBackColor(Color::WHITE);
+    score.get()->setTextPosition(0, 0);
+    _map.setText("score", *score.get());
+    _textBlocks.push_back(std::move(score));
+    std::unique_ptr<ATextBlock> title = std::make_unique<ATextBlock>();
+    title.get()->setText("S N A K E");
+    title.get()->setColor(Color::BLACK);
+    title.get()->setBackColor(Color::WHITE);
+    title.get()->setTextPosition(0, 13);
+    _map.setText("title", *title.get());
+    _textBlocks.push_back(std::move(title));
     _initSnake();
     setMap(_map);
 }
@@ -42,7 +56,7 @@ void acd::Snake::_initSnake()
 {
     _map.removeText("gameOver");
     for (std::size_t x = 1; x < 30; x++)
-        for (std::size_t y = 1; y < 30; y++)
+        for (std::size_t y = 2; y < 31; y++)
             _map.removeBlock(x, y);
     _snake = std::vector<std::pair<int, int>>();
     _direction = 0;
@@ -55,16 +69,17 @@ void acd::Snake::_initSnake()
     for (std::size_t c = 0; c < _snake.size(); c++)
         _map.setBlock(_snake[c].first, _snake[c].second, *_blocks[0].get());
     setScore(0);
+    _map.getText("score").setText("Score: " + std::to_string(getScore()));
     setMap(_map);
 }
 
 void acd::Snake::_setGameOver()
 {
     std::unique_ptr<ATextBlock> gameOver = std::make_unique<ATextBlock>();
-    gameOver.get()->setText("GAME OVER");
+    gameOver.get()->setText("GAME  OVER");
     gameOver.get()->setColor(Color::BLACK);
     gameOver.get()->setBackColor(Color::RED);
-    gameOver.get()->setTextPosition(13, 15);
+    gameOver.get()->setTextPosition(15, 13);
     _map.setText("gameOver", *gameOver.get());
     _textBlocks.push_back(std::move(gameOver));
     _isGameOver = true;
@@ -133,7 +148,7 @@ acd::updateType_t acd::Snake::update(Input latestInput)
     _snake.pop_back();
     _movedOnce = true;
     for (std::size_t c = 0; c < _snake.size(); c++) {
-        if (_snake[c].first == 0 || _snake[c].first == 30 || _snake[c].second == 0 || _snake[c].second == 30) {
+        if (_snake[c].first == 0 || _snake[c].first == 30 || _snake[c].second == 1 || _snake[c].second == 31) {
             _setGameOver();
         } else {
             _map.setBlock(_snake[c].first, _snake[c].second, *_blocks[0].get());
@@ -147,10 +162,11 @@ acd::updateType_t acd::Snake::update(Input latestInput)
     if (_snake[0].first == _food.first && _snake[0].second == _food.second) {
         _snake.push_back(std::pair<int, int>(_snake[_snake.size() - 1].first, _snake[_snake.size() - 1].second));
         _map.removeBlock(_food.first, _food.second);
-        _food = std::pair<int, int>(rand() % 29 + 1, rand() % 29 + 1);
+        _food = std::pair<int, int>(rand() % 29 + 1, rand() % 29 + 2);
         _map.setBlock(_food.first, _food.second, *_blocks[2].get());
         setScore(getScore() + 1);
     }
+    _map.getText("score").setText("Score: " + std::to_string(getScore()));
     setMap(_map);
     _lastUpdate = std::chrono::system_clock::now();
     return acd::updateType_t::UPDATE_NONE;
@@ -170,4 +186,3 @@ void acd::Snake::restart()
 {
 
 }
-
